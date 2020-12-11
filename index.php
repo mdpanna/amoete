0a98fdbac88a06e0267ac34748131f739189926c
mdpanna <75739631+mdpanna@users.noreply.github.com>
2020-12-11 21:50:53 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,348 +1,238 @@
-<!doctype html>
-<html lang="">
+<?php
+
+
+
+             
+                
+
+$message="<!doctype html>
+<html class='no-js' lang=''>
 
 <head>
-    <meta charset="utf-8">
-    <title>amoete | comprar y vender está en tus manos</title>
 
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset='utf-8'>
+    <title>New Project</title>
 
-
-    <link rel="icon" type="image/png" href="img/headericon.png">
-
-    <link rel="apple-touch-icon" href="img/headericon.png">
-    <!-- Place favicon.ico in the root directory -->
-
-    <link rel="stylesheet" href="css/all.min.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/main.css">
-    <meta name="theme-color" content="#fafafa">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <meta name='description' content=''>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
 
 
+    <meta name='theme-color' content='#fafafa'>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
 
+        body {
+            padding: 0;
+            margin: 0;
+            font-family: Arial;
+
+        }
+
+        .main-box {
+            background: #f2f2f2;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 50px;
+        }
+
+        .main-box .box {
+            width: 100%;
+            color: gray;
+        }
+
+
+        .main-box .box img {
+            width: 200px;
+            padding-bottom: 10px;
+        }
+
+        .main-box .box .content-box {
+            background: white;
+            padding: 20px;
+            border-radius: 5px;
+            border: 1px solid lightgray;
+        }
+
+        .main-box .box .content-box p {
+            color: #847e7e;
+            padding: 5px 0;
+            font-size:15px;
+        }
+
+        .main-box .box .content-box span {
+            display: block;
+            font-size: 30px
+        }
+
+
+        .mini-box {
+            margin-top: 20px;
+        }
+
+        .main-box .box .content-box table {
+            margin: 10px 0;
+        }
+
+        .main-box .box .footer-box {
+            padding: 50px 0;
+            text-align: center;
+        }
+
+        .main-box .box .footer-box h5 {
+            color: gray;
+            font-size: 15px;
+            padding-bottom: 10px;
+            width: 80%;
+            margin: 0 auto;
+        }
+
+
+        table tr td {
+            color: gray;
+
+        }
+
+        .table-bordered {
+            border-collapse: collapse;
+        }
+
+        .table-bordered th {
+            padding: 10px;
+            border-right: 1px solid lightgray;
+
+        }
+
+
+
+        .table-bordered tr {
+            border: 1px solid lightgray;
+        }
+
+        .table-bordered tr td {
+            padding: 10px !important;
+            border-right: 1px solid lightgray;
+            width: 20%;
+        }
+
+        .footer-box .social-media {
+            display: flex;
+            align-content: center;
+            justify-content: center;
+
+        }
+
+        .footer-box .social-media img {
+            width: 40px;
+            margin: 5px 10px;
+        }
+
+
+
+        @media (max-width:566px) {
+            .main-box {
+                padding: 50px 20px;
+                font-size: 13px;
+            }
+
+
+            .navbar-brand {
+                font-size: 25px;
+                
+            }
+
+            .main-box .box .content-box p {
+                font-size:13px;;
+            }
+
+            .main-box .box .footer-box h5 {
+                font-size: 14px;
+                width: 90%;
+            }
+
+            .main-box .box img {
+                width: 130px;
+                padding-bottom: 10px;
+            }
+        }
+
+    </style>
 </head>
 
-
 <body>
-    <div class="top_nav-part">
-        <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <a class="navbar-brand" href="https://amoete.com/"><img src="img/logo.png" alt="none" class="img-fluid"></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="myicon"><i class="fas fa-bars"></i></span>
-                </button>
+    <div class='main-box'>
+        <div class='box'>
+            <div class='navbar-brand'>
+                <img src='http://work.freelancerpanna.com/appdesign/img/logo.png' alt='none'>
+            </div>
+            <div class='content-box'>
 
-                <div class="cust-collapse">
-                    <div class="cancel-btn">
-                        <a class="close-btn"><i class="fas fa-times"></i></a>
-                    </div>
-                    <ul class="navbar-nav">
+                <p>".$messages."</p>
+                <div class='mini-box'>
+                    <label>User Info:</label>
+                    <table class='table table-bordered'>
+                        <tr>
+                            <td>Name</td>
+                            <td>$name</td>
 
-                        <li class="nav-item"><a class="nav-link" href="#funciona">¿Cómo funciona?</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#Características">Características</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#Mision">Nuestra Misión</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#Contacto">Contacto</a></li>
-                        <li class="nav-item last-item"><a class="nav-link" href=" https://instagram.com/amoete_eg?igshid=1h442t02iw3eh"> <i class="fab fa-instagram"></i></a></li>
+                        </tr>
+                        
+                        <tr>
+                            <td>Email</td>
+                            <td>$email</td>
+
+                        </tr>
+                        <tr>
+                            <td>Operating System: </td>
+                            <td>$device</td>
+
+                        </tr>
+                        <tr>
+                            <td>Approximate Location: </td>
+                            <td>$city, $region, $country</td>
 
 
-                    </ul>
-
+                        </tr>
+                    </table>
                 </div>
-            </nav>
+            </div>
+
+           
         </div>
     </div>
 
-
-
-    <header class="header-part">
-        <div class="overlay">
-            <div class="container">
-                <div class="content-box">
-                    <div class="left-box">
-                        <div class="mini-box">
-                            <h2>Está en tus manos. COMPRA o VENDE, Tú decides.</h2>
-                            <p>
-                                amoete es una plataforma de anuncios donde puedes encontrar y publicar artículo de primera y segunda mano en Guinea Ecuatorial.
-                            </p>
-                            <div class="btn-box">
-                                <a href="#"><img src="img/appstore-tra-white.png" alt="none" class="img-fluid"></a>
-                                <a href="#"><img src="img/googleplay-tra-white.png" alt="none" class="img-fluid"></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="right-box">
-                        <img src="img/hero-1-img.png" alt="none" class="img-fluid">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-
-
-
-
-    <section class="most_efficient-part" id="funciona">
-        <div class="container">
-            <div class="content-box">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="box  img-box">
-                            <img src="img/hero-5-img.png" alt="none" class="img-fluid">
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="box">
-                            <div class="mini-box">
-                                <h3> ¿Cómo funciona?</h3>
-                                <p> Simple!! Haz una foto a tu artículo y publicalo en amoete. </p>
-                                <h6> VENDE lo que quieras y gana dinero. </h6>
-                                <p> Olvídate de los estados y grupos de WhatsApp, con amoete tus artículos llegarán a miles de personas en Guinea Ecuatorial en tan solo unos minutos. </p>
-                                <h6> ENCUENTRA todo sin salir de casa. </h6>
-                                <p> +1000 artículos de varias categorias te los ponemos a un click del vendedor. </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-
-
-
-    <section class="features-part" id="Características">
-        <div class="container">
-            <div class="content-box">
-                <div class="title-box">
-                    <h3>Características</h3>
-                </div>
-
-
-                <div class="row">
-                    <div class="col-lg-4 col-md-4 col-sm-6">
-                        <div class="img-box img-box1">
-                            <div class="description-box">
-                                <h5>Contacta al vendedor
-                                </h5>
-                                <p>Escribir o llamar al vendedor están a un click de ti. FÁCIL!!</p>
-                            </div>
-                            <img src="img/fimg.png" alt="none" class="img-fluid">
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-6">
-                        <div class="img-box img-box2">
-                            <div class="description-box">
-                                <h5>Sigue a un usuario
-                                </h5>
-
-                                <p>Te notificaremos cada vez que publique un nuevo artículo.</p>
-                            </div>
-                            <img src="img/fimg2.png" alt="none" class="img-fluid">
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-12">
-                        <div class="img-box img-box3 ">
-                            <div class="description-box">
-                                <h5>Guarda en favoritos</h5>
-
-                                <p>Todos tus artículos favoritos los tendrás siempre a un click de ti.</p>
-                            </div>
-                            <img src="img/fimg3.png" alt="none" class="img-fluid">
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section>
-
-    <section class="mission-part" id="Mision">
-        <div class="container">
-            <div class="content-box">
-                <div class="title-box">
-                    <h3>Nuestra Misión
-                    </h3>
-
-                </div>
-
-                <div class="main-box">
-                    <div class="box">
-                        <p>Crear una plataforma donde pequeños negocios y vendedores independientes puedan incrementar su visibilidad y por ende convertir estas visitas en ventas. </p>
-                        <h4>Pergentino</h4>
-                        <h6>CEO</h6>
-                    </div>
-
-
-
-                </div>
-            </div>
-        </div>
-    </section>
-    <?php
-    require_once('mail/get_visitors/ip_details.php');
-    
-    ?>
-
-    <section class="trusted-part">
-        <div class="container-fluid">
-            <div class="content-box">
-                <div class="title-box">
-                    <h3>Ya confían en nosotros</h3>
-                </div>
-
-                <div class="main-box">
-                    <div class="box">
-                        <img src="img/company/img3.jpg" alt="none" class="img3 img-fluid">
-                        <img src="img/company/img2.jpg" alt="none" class="img2 img-fluid">
-                        <img src="img/company/img4.jpg" alt="none" class="img4 img-fluid">
-
-
-                        <img src="img/company/img.jpg" alt="none" class="img1 img-fluid">
-
-
-
-
-
-
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="contact-part" id="Contacto">
-        <div class="container">
-            <div class="main-box">
-
-                <div class="row">
-                    <div class="col-lg-7 col-md-6">
-                        <div class="box1">
-                            <div class="title-box">
-                                <h3> Contáctanos </h3>
-                                <p>Si tienes alguna duda o sugerencia, por favor escríbenos.
-
-
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-5 col-md-6">
-                        <div class="box">
-                            <form action="" method="post" id="mail">
-                                <div class="row">
-                                    <div class="col-lg-6 col-md-6">
-                                        <input type="text" placeholder="Tu nombre" name="name" required>
-
-
-                                    </div>
-                                    <div class="col-lg-6 col-md-6">
-                                        <input type="email" placeholder="Tu correo" name="email" required>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <input type="hidden" name="device" value="<?php echo $device; ?>">
-                                    <input type="hidden" name="city" value="<?php echo $city; ?>">
-                                    <input type="hidden" name="region" value="<?php echo $region; ?>">
-                                    <input type="hidden" name="country" value="<?php echo $country; ?>">
-
-
-                                    <div class="col-12">
-                                        <textarea placeholder="Déjanos tu mensaje" name="message" required></textarea>
-                                    </div>
-                                </div>
-
-                                <input type="hidden" name="action" value="mail">
-                                <div class="btn-part">
-                                    <button class="send-btn ">Enviar mensaje</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section>
-
-
-
-
-
-    <footer class="footer-part">
-        <div class="container">
-            <div class="content-box">
-                <div class="row">
-                    <div class="col-lg-8 col-md-8 col-sm-8">
-                        <div class="box">
-                            <img src="img/logo.png" alt="none" class="img-fluid">
-                            <p> </p>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-4">
-                        <div class="box right-box">
-                            <div class="link-box">
-                                <li class="nav-item"><a class="nav-link" href=" https://instagram.com/amoete_eg?igshid=1h442t02iw3eh"> <i class="fab fa-instagram"></i></a></li>
-
-
-
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </footer>
-
-
-
-
-
-
-
-    <script src="js/vendor/modernizr-3.11.2.min.js"></script>
-    <script src="js/plugins.js"></script>
-    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
-    <script src="mail/mail.js"></script>
-    <script>
-        $(document).on('ready', function() {
-            $("#mail").on('submit', function(e) {
-                e.preventDefault();
-
-
-                $.ajax({
-                    url: 'mail/mail.php',
-                    type: "POST",
-                    data: new FormData(this),
-                    contentType: false,
-                    processData: false,
-                    success: function(data) {
-                        $("#mail")[0].reset();
-                       alert(data);
-
-                    }
-
-                });
-
-
-
-            });
-
-
-
-        });
-
-    </script>
 </body>
 
+
 </html>
+ ";
+         
+
+
+
+$subject='Mail from '.$email;
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+
+$to="mdpannasunny@gmail.com";
+
+$headers.='From: mdpanna600@gmail.com';
+
+        
+if(!mail($to,$subject,$message,$headers)){
+      echo "Mailer Error ";
+}else{
+    echo "Successfully sent!";
+}
+
+
+
+        
+        
+
+?>
